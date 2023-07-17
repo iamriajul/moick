0e07d90323cf89969bab7a21fd006206a7fead3e
diecodev <diegodiaz05@gmail.com>
2023-07-17 06:30:12 -0500

    refactor(qwik): :art: Update code style

--- packages/qwik/src/utils/index.ts
+++ packages/qwik/src/utils/index.ts
@@ -1,5 +1,13 @@
-export * from "./constants";
-export * from "./observer";
+export {
+  DEFAULT_TOAST_OPTIONS,
+  DEFAULT_WRAPPER_OPTIONS,
+  GAP,
+  TIME_BEFORE_UNMOUNT,
+  TOAST_LIFETIME,
+  TOAST_WIDTH,
+  VIEWPORT_OFFSET,
+} from "./constants";
+export { Observer, generateId } from "./observer";
 
 export const createOptionsObject = <T>(objOne: T, objTwo: Partial<T>): T => {
   const obj: Record<string, any> = {};
